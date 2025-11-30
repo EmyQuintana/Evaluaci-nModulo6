8271843b0f014d44e3a44f54754320d3f1a48b33
Emy Quintana <ing.quintanaemily@gmail.com>
2025-11-30 16:44:55 -0300

    Modificacion templates

--- gestor_tareas/gestor_tareas/settings.py
+++ gestor_tareas/gestor_tareas/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'gestor_tareas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -97,9 +97,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
